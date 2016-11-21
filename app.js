e062b36c25c21701058f2cf53a076244f299495b
Stephanie Cray <stephanie.cray@yahoo.com>
2016-11-21 00:23:04 -0500

    added player2,score keeper,and css to table

--- app.js
+++ app.js
@@ -5,3 +5,94 @@ $('p').hover(function(){
 	});
 
 //$('.row').hide(function(){$('h2').hide();});
+
+document.getElementById('myButton').onclick = function () {
+        //location.href = "index2.html";
+   var player1 = prompt("Player 1: Please enter your last name:")
+   var player2 = prompt("Player 2: Please enter your last name:")
+
+   if ((player1)&&(player2)) // when player enters name
+   	  {
+   	  	$('.all').hide(function()
+   			{
+   				$('all').hide();
+   				//$('#player1').html(player1 + ' FAMILY'); //adds last name to family
+   		        //$('#player2').html(player2 + ' FAMILY'); 
+   			}) 
+
+
+		$('.gameBoard').show(function(){//activates game show//
+			$('').show();
+			$('#player1').html(player1 + ' FAMILY'); //adds last name to family
+   		        $('#player2').html(player2 + ' FAMILY'); 
+
+
+{console.log('this is also true');}
+
+ })
+
+} else if ((player1) || (player2) === "")// when player leaves it blank.
+   	{
+   		alert("Please enter last name to proceed to the game.");
+   console.log('this is true');
+   	}
+
+   	 else  {
+   	console.log('nothing was true'); // when cancelling game
+   }
+
+   };
+
+$(function(){
+var allQuestions=
+
+   {
+   	text:[
+
+	{que: 'If a witch was not paying attention to where she was flying, name something she might crash into?'},
+    
+    {que: 'Name a sport where many players end up in jail?'},
+
+    {que: 'Name an animal that we eat that would never eat us?'},
+
+    {que: 'Name a fruit that you never buy just one of?'},
+
+    {que: 'Name an illness that kids give to each other?'}
+
+ ]};
+
+ allQuestions.count= 0;
+
+var query = {
+    render: function() {       
+        if (allQuestions.length >= allQuestions.length) {
+        
+          $("#answers","userAnswer").remove();  
+        }
+      else {      
+        var allQuestions = allQuestions.text[allQuestions.count].que;
+        $("#listQuestion").html(allQuestions);
+        $("#userAnswer").val("");        
+      }
+    }
+  };
+  
+  query.render();
+   
+  $("#userAnswer").click(function () {
+    if ($("#answers").val() == "") {
+      alert("Please enter response for continue");
+      return false;
+    }
+    else {
+      allQuestions.text[allQuestions.length].response = $("#userAnswer").val();
+      ++allQuestions.length;
+      query.render();
+    }
+  });
+  
+  
+ // Handler for .ready() called.
+ });
+
+
--- app2.js
+++ app2.js
@@ -1,44 +1,266 @@
 
 
-document.getElementById('myButton').onclick = function () {
+/*document.getElementById('myButton').onclick = function () {
         //location.href = "index2.html";
-   var player = prompt("Please enter your last name:")
-   	if (player === "")// when player leaves it blank.
-   	{
-   		alert("Please enter last name to proceed to the game.");
-   	}
+   var player1 = prompt("Player 1: Please enter your last name:")
+   var player2 = prompt("Player 2: Please enter your last name:")
 
-   	  else if (player) // when player enters name
+   	//if ((player1) && (player2) === "")// when player leaves it blank.
+   	//{
+   	//	alert("Please enter last name to proceed to the game.");
+   //console.log('this is true');
+   	//}
+
+   	       if ((player1)&&(player2)) // when player enters name
    	  {
    	  	$('.all').hide(function()
    			{
    				$('all').hide();
-   				$('#user').html(player + ' FAMILY'); //adds last name to family
+   				//$('#player1').html(player1 + ' FAMILY'); //adds last name to family
+   		        //$('#player2').html(player2 + ' FAMILY'); 
    			}) 
 
 
 		$('.gameBoard').show(function(){//activates game show//
-			$('h3').show();
+			$('').show();
+			$('#player1').html(player1 + ' FAMILY'); //adds last name to family
+   		        $('#player2').html(player2 + ' FAMILY'); 
+
+
+{console.log('this is also true');}
+
 			//$('user').html(player);
-		
+		   		//var question1 = prompt("If a witch was not paying attention to where she was flying, name something she might crash into?");
+   	        })
 
+} else if ((player1) || (player2) === "")// when player leaves it blank.
+   	{
+   		alert("Please enter last name to proceed to the game.");
+   console.log('this is true');
+   	}
 
-		   		console.log('hey');
-   	})	
-	}
-
-    else  {
+//prompt("If a witch was not paying attention to where was flying, name something she might crash into?")
+	
+	
+  else  {
    	console.log('nothing was true'); // when cancelling game
    }
-   
+
+   };
+
+   //var question2= ('Name a sport where many players end up in jail?');
+
+$('.question2').hide(function(){
+$('.question2').hide();
+
+	$('.question3').hide(function(){
+		$('question3').hide();
+	
+	$('.question4').hide(function(){
+		$('.question4').hide();
+
+	$('.question5').hide(function(){
+		$('.question5').hide();
+	})
+
+	
+})
+})
+})
+
+
+
+
+//let question2= ('Name a sport where many players end up in jail?');
+
+
+/*$('.question2').show(function(){
+	$('.question2').show();
+
+	$('.question1').hide(function(){
+		$('.question1').hide();
+
+		$('.question2').show(function(){
+			$('.question2').show();
+		})
+	})
+})
+
+//};*/
+
+
+
+/*document.getElementById('userAnswer').onclick= function(){
+
+var queAns1 = [
+	'tree',
+	'building',
+	'pole',
+	'powerline',
+	'plane',
+	'bird',
+	'bugs'
+];
+
+for(var i=0; i < queAns1.length; i+= 1)
+{
+
+if (('.question1') == queAns1[i]); {
+	
+	showInput();
+}
+
+	// else if (('.question2') == queAns1[i]);{
+	//	showInput();
+	//}
+
+
+/*$('.question1').hide(function(){
+	$('.question1').hide();
+
+	 $('.question2').show(function(){
+		$('.question2').show();
+	})
+})*/
+/*}
+
 };
+
+
+
+function showInput() {
+        document.getElementById('#table1').innerHTML = 
+                    document.getElementById("answers").value;
+    }
+
+function showInput() {
+        document.getElementById('#table2').innerHTML = 
+                    document.getElementById("answers").value;
+    }
+
+
+	/*var queAns1 = [
+	'tree',
+	'building',
+	'pole',
+	'powerline',
+	'plane',
+	'bird',
+	'bugs'
+];
+
+for(var i=0; i < queAns1.length; i+= 1){
+
+if (question1 == queAns1[i]); {
+	alert('You are right');
+}
+
+
+
+
+/*var que1Ans = [
+	'tree',
+	'building',
+	'pole',
+	'powerline',
+	'plane',
+	'bird',
+	'bugs'
+];
+
+if (question1 === que1Ans[i]){
+	alert('You are right');
+}
+
+
+
+
+//prompt("If a witch was not paying attention to where she was flying, name something she might crash into?");
+
+$('p').fadeOut();
+$('.question1').fadeIn();
+
+
+//function togglediv(questions){
+//	$('.questions').toggle()
+//};
+//$('.questions').toggle(function()
+//{
+
+//$('.questions').toggle();
+//});
+
+//$('.question1').show(function()
+//{
+//	$('.question1').show();
+//});
+
+
+
+
+
+
+
+
+
+document.getElementById('userAnswer').onclick = function question1 ()
+
+{
+	//var answers1= 5;
+
+var value = document.getElementById("question1").value;
+	
+	if('question1' === 5) {
+
+alert('right');
+//$('#question1').hide(function() 
+
+//{
+
+		//$('#question1').hide();
+//});
+	}
+
+//else {
+//	alert('Wrong!');
+//}
+
+};
+
+
+//$('#question1').show(function()
+//{
+//	$('#question1').show();
+//})
+
+
+
+var startTime = function(){
+	var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('timer').innerHTML =
+    h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 500);
+}
+//function checkTime(i) {
+   // if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    //return i;
+//};
+
+document.getElementById('userAnswer').onclick = function (){
+	if (var player 
+}
 
 //$('.gameBoard').show(function(){
 //	$('h3').show();
 //});
 
 
-/*document.getElementById('myButton').onclick = function () {
+document.getElementById('myButton').onclick = function () {
 	let player = prompt('Please enter your last name:')
 	{ 
 		if (player != null){location.href = "index2.html";
@@ -63,9 +285,9 @@ document.getElementById('myButton').onclick = function () {
 
     	
 
-    		/*if (player != null)}
-    		{
-    } else {prompt("Please enter last name to proceed.")}
-    		}
-    	};*/
+    		//if (player != null)}
+    		//{
+    //} else {prompt("Please enter last name to proceed.")}
+    		//}
+    	//};
   
